3048e3fc1bd781d38fcc3d611d94b4bbc7c94999
juliemartins1 <166827680+juliemartins1@users.noreply.github.com>
2025-05-29 19:48:39 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,27 +1,26 @@
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.util.Date;
-import java.util.List;
+
+
 
 public class Main {
     public static void main(String[] args) {
         // 1. Criando pacientes
         Paciente paciente1 = new Paciente(
-            "João", "98765432100", "53988888888",
+            "João", "98765432100", "53988888888","joaoa@gmail.com",
             LocalDate.of(1991, 10, 13),
             TipoGenero.MASCULINO, TipoSanguineo.A_positivo
         );
 
        
         // 2. Criando médicos e abrindo agenda
-        Medico medico1 = new Medico("Carla", "12345678900", "53999999999", "CRM1234", "Cardiologia");
+        Medico medico1 = new Medico("Carla", "12345678900", "53999999999", "CRM1234", "Cardiologia","carla@gmail.com");
         medico1.abrirAgenda(LocalDate.of(2025, 6, 1));
 
         
         try {
             // 3. Agendando consultas
-            LocalDateTime dataHora = LocalDateTime.of(2025, 6, 1, 10, 0);
+            LocalDateTime dataHora = LocalDateTime.of(2025, 6, 1, 17, 0);
             medico1.agendar(dataHora.toLocalDate(), dataHora.toLocalTime());
             Consulta consulta1 = new Consulta(dataHora, TipoConsulta.ROTINA, medico1, paciente1);
             paciente1.adicionarConsulta(consulta1);
@@ -33,13 +32,9 @@ public class Main {
 
            
             // 5. Criando técnico e marcando exame
-            Tecnico tecnico = new Tecnico("Pedro", "11122233344", "53998887777");
+            Tecnico tecnico = new Tecnico("Roberto", "11122233344", "53998887777","pedro@gmail.com");
+            
 
-            System.out.println("=== Informações do Responsável pelo Exame ===");
-            IResponsavel responsavel = tecnico; 
-            System.out.println("Nome: " + responsavel.getNome());
-            System.out.println("CPF: " + responsavel.getDocumento());
-            System.out.println("Tipo: " + responsavel.getTipoResponsavel());
             LocalDateTime dataExame = LocalDateTime.of(2025, 6, 1, 14, 0);
             Exames exame = new Exames("ECG", dataExame, tecnico,paciente1);
             paciente1.adicionarExame(exame);
@@ -50,6 +45,12 @@ public class Main {
             for (Consulta c : paciente1.getHistoricoConsultas()) {
                 System.out.println("nome paciente: "+ paciente1.getNome()+"\n- Consulta em: " + c.agendar());
             }
+
+            System.out.println("=== Informações do Responsável pelo Exame ===");
+            IResponsavel responsavel = tecnico;
+            System.out.println("Nome: " + responsavel.getNome());
+            System.out.println("CPF/CRM: " + responsavel.getDocumento());
+            System.out.println("Tipo: " + responsavel.getTipoResponsavel());
             
 
                 System.out.println("=== Exames ===");
@@ -63,7 +64,7 @@ public class Main {
          
             
             System.out.println("=== Prontuário Paciente ===");
-            System.out.println("Nome: " + paciente1.getNome());
+           
             exibirProntuario(consulta1.getProntuario());
 
           
